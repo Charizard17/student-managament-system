f8beece8c2d665631a8fc6880d157ed0594932a0
esdu <3723esad@gmail.com>
2020-09-21 11:43:08 +0200

    initial commit

---
+++ grades.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="styleheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body class="p-3">
+
+    <form name="Student" method="POST" action="studentdb.php">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2 class="bg-secondary font-weight-bold text-white"><b>Course Information</b></h2>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </form>
+
+</body>
+</html>
